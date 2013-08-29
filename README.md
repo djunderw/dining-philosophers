2932041b8bfca9a7a5d9cfa936593da3bfc5993c
Daniel Underwood <daniel.underwood@ncsu.edu>
2013-08-29 17:08:23 -0400

    Added custom semaphore

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ dining-philosophers
 
 Parallel solution to the dining-philosophers problem in Python using the threading module
 
-This is a very simple example of using the threading module to create parallel programs in Python.  
+This is a very simple example of using the threading module to create parallel programs in Python. It demonstrates basic locking functions and how to create a custom semaphore to manage concurrent threads.
--- dining-philosophers.py
+++ dining-philosophers.py
@@ -3,62 +3,82 @@
 '''
 Created on Mar 20, 2011
 
-@author: Daniel Underwood
+@author: Daniel Underwood (and Dr. John Baugh)
 '''
 
 
-# symmetric implementation of dining philosophers
+# Symmetric solution to the "dining philosophers"
+# problem. Uses a semaphore as the "butler" to avoid
+# deadlock.
 
 import sys
 import threading
 import time
 
-class ChopStick:
+class Semaphore(object):
+
+    def __init__(self, initial):
+        self.lock = threading.Condition(threading.Lock())
+        self.value = initial
+
+    def up(self):
+        with self.lock:
+            self.value += 1
+            self.lock.notify()
+
+    def down(self):
+        with self.lock:
+            while self.value == 0:
+                self.lock.wait()
+            self.value -= 1
+
+class ChopStick(object):
+
     def __init__(self, number):
         self.number = number           # chop stick ID
         self.user = -1                 # keep track of philosopher using it
-        self.lock = threading.Lock()
-    
-    def get_lock(self):
-        return self.lock
+        self.lock = threading.Condition(threading.Lock())
+        self.taken = False
 
-    def take(self, user):         # just for I/O, not used for synchronization
-        if self.user == -1:
+    def take(self, user):         # used for synchronization
+        with self.lock:
+            while self.taken == True:
+                self.lock.wait()
             self.user = user
+            self.taken = True
             sys.stdout.write("p[%s] took c[%s]\n" % (user, self.number))
-        else:
-            sys.stdout.write("p[%s] cannot take c[%s] (taken by c[%s])\n" % 
-                             (user, self.number, self.user))
-            sys.exit(1)
+            self.lock.notifyAll()
 
-    def drop(self, user):         # just for I/O, not used for synchronization
-        if self.user == user:
+    def drop(self, user):         # used for synchronization
+        with self.lock:
+            while self.taken == False:
+                self.lock.wait()
             self.user = -1
+            self.taken = False
             sys.stdout.write("p[%s] dropped c[%s]\n" % (user, self.number))
-        else:
-            sys.stdout.write("p[%s] cannot drop c[%s] (taken by c[%s])\n" % 
-                             (user, self.number, self.user))
-            sys.exit(1)
+            self.lock.notifyAll()
 
 
 class Philosopher (threading.Thread):
-    def __init__(self, number, left, right):
+
+    def __init__(self, number, left, right, butler):
         threading.Thread.__init__(self)
         self.number = number            # philosopher number
         self.left = left
         self.right = right
+        self.butler = butler
 
     def run(self):
         for i in range(20):
-            time.sleep(0.1)             # think
-            with self.left.get_lock():
-                self.left.take(self.number)
-#                time.sleep(0.1)         # yield makes deadlock more likely
-                with self.right.get_lock():
-                    self.right.take(self.number)
-                    time.sleep(0.1)     # eat
-                    self.right.drop(self.number)
-                self.left.drop(self.number)
+            self.butler.down()              # start service by butler
+            time.sleep(0.1)                 # think
+            self.left.take(self.number)     # pickup left chopstick
+            time.sleep(0.1)                 # (yield makes deadlock more likely)
+            self.right.take(self.number)    # pickup right chopstick
+            time.sleep(0.1)                 # eat
+            self.right.drop(self.number)    # drop right chopstick
+            self.left.drop(self.number)     # drop left chopstick
+            self.butler.up()                # end service by butler
         sys.stdout.write("p[%s] finished thinking and eating\n" % self.number)
 
 
@@ -66,11 +86,14 @@ def main():
     # number of philosophers / chop sticks
     n = 5
 
+    # butler for deadlock avoidance (n-1 available)
+    butler = Semaphore(n-1)
+
     # list of chopsticks
     c = [ChopStick(i) for i in range(n)]
 
     # list of philsophers
-    p = [Philosopher(i, c[i], c[(i + 1) % n]) for i in range(n)]
+    p = [Philosopher(i, c[i], c[(i+1)%n], butler) for i in range(n)]
 
     for i in range(n):
         p[i].start()
